9e03d6ce1067d394e21075f38e54ee833057f461
anuj <anuj@C40LAP-10090.india.rsystems.com>
2023-04-03 22:06:37 +0530

    johnsmil-redux-02-cart-item - reducer functions, createAsyncThunk.

--- starter/src/components/CartItem/index.jsx
+++ starter/src/components/CartItem/index.jsx
@@ -1,8 +1,55 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 
-const CartItem = () => {
+import { ChevronDown, ChevronUp } from "../../icons";
+import { decrease, increase, removeItem } from "../../features/cart/cartSlice";
+
+const CartItem = props => {
+  const dispatch = useDispatch();
+  const {id, img, title, price, amount} = props;
+
   return (
-    <div>Cart Item</div>
+    <article className="cart-item">
+      <img src={img} alt={title} />
+
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+
+        <button
+          className="remove-btn"
+          onClick={() => {
+            dispatch(removeItem(id));
+          }}
+        >remove</button>
+      </div>
+
+      <div>
+        <button
+          className="amount-btn"
+          onClick={() => {
+            dispatch(increase({id}));
+          }}
+        >
+          <ChevronUp />
+        </button>
+
+        <p className="amount">{amount}</p>
+
+        <button
+          className="amount-btn"
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItem(id));
+              return;
+            }
+            dispatch(decrease({id}));
+          }}
+        >
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
   );
 };
 
